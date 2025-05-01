83ea0f45cb160e2b12b23ebc2a023cf0468fd332
peanut4556 <liuj7533@gmail.com>
2025-05-01 23:25:00 +0800

    Added a new pre-registration terms page.

---
+++ static/js/modal.js
@@ -0,0 +1,22 @@
+function openModal() {
+  var modal = document.getElementById('confirmModal');
+  modal.classList.add('show');
+}
+
+function closeModal() {
+  var modal = document.getElementById('confirmModal');
+  modal.classList.remove('show');
+  setTimeout(function () {
+    modal.style.display = 'none'; // Delay hiding for smooth animation
+  }, 300);
+}
+
+function proceedRegistration() {
+  // User confirms and jumps to the registration page
+  location.href = "register.html";
+}
+
+function cancelRegistration() {
+  // Cancel registration and jump back to home page
+  location.href = "homepage.html";
+}
